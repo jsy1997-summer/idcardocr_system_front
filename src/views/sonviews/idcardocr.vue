b5dcce180d39dc8d99de721d6be8d1d8f8bdbea7
贾素银 <jiasy@sincinfo.cn>
2021-06-23 09:49:58 +0800

    “单文件识别的加密解密功能完成”

--- src/views/sonviews/idcardocr.vue
+++ src/views/sonviews/idcardocr.vue
@@ -130,7 +130,8 @@ var took = sessionStorage.getItem('tokenKey');
           
             file.url = reader.result
             _this.uploadList.push(file)
-            _this.image_base64=file.url;   
+            _this.image_base64=file.url; 
+          
            
         }  
        
@@ -139,6 +140,9 @@ var took = sessionStorage.getItem('tokenKey');
         this.$axios({
           method:'post',
           url:'http://'+this.addre+'/getkey/',
+          data:{
+            "asw":"这里"
+          },
           header:{
             'Content-Type':"application/x-www-form-urlencoded",
             }
@@ -156,29 +160,30 @@ var took = sessionStorage.getItem('tokenKey');
                this.private_key = this.private_key.replace("b'","")
                this.private_key = this.private_key.replace("'","")
                this.private_key = this.private_key.replace(/\\n/g, " \n ");
-
-               this.image_base64 = "豫章故郡，洪都新府。星分翼轸，地接衡庐。襟三江而带五湖，控蛮荆而引瓯越。物华天宝，龙光射牛斗之墟；人杰地灵，徐孺下陈蕃之榻。雄州雾列，俊采星驰。台隍枕夷夏之交，宾主尽东南之美。都督阎公之雅望，棨戟遥临；宇文新州之懿范，襜帷暂驻。十旬休假，胜友如云；千里逢迎，高朋满座。腾蛟起凤，孟学士之词宗；紫电青霜，王将军之武库。家君作宰，路出名区；童子何知，躬逢胜饯。时维九月，序属三秋。潦水尽而寒潭清，烟光凝而暮山紫。俨骖騑于上路，访风景于崇阿；临帝子之长洲，得天人之旧馆。层峦耸翠，上出重霄；飞阁流丹，下临无地。鹤汀凫渚，穷岛屿之萦回；桂殿兰宫，即冈峦之体势。披绣闼，俯雕甍，山原旷其盈视，川泽纡其骇瞩。闾阎扑地，钟鸣鼎食之家；舸舰弥津，青雀黄龙之舳。云销雨霁，彩彻区明。落霞与孤鹜齐飞，秋水共长天一色。渔舟唱晚，响穷彭蠡之滨；雁阵惊寒，声断衡阳之浦。遥襟甫畅，逸兴遄飞。爽籁发而清风生，纤歌凝而白云遏。睢园绿竹，气凌彭泽之樽；邺水朱华，光照临川之笔。四美具，二难并。穷睇眄于中天，极娱游于暇日。天高地迥，觉宇宙之无穷；兴尽悲来，识盈虚之有数。望长安于日下，目吴会于云间。地势极而南溟深，天柱高而北辰远。关山难越，谁悲失路之人？萍水相逢，尽是他乡之客。怀帝阍而不见，奉宣室以何年？嗟乎！时运不齐，命途多舛。冯唐易老，李广难封。屈贾谊于长沙，非无圣主；窜梁鸿于海曲，岂乏明时？所赖君子见机，达人知命。老当益壮，宁移白首之心？穷且益坚，不坠青云之志。酌贪泉而觉爽，处涸辙以犹欢。北海虽赊，扶摇可接；东隅已逝，桑榆非晚。孟尝高洁，空余报国之情；阮籍猖狂，岂效穷途之哭！勃，三尺微命，一介书生。无路请缨，等终军之弱冠；有怀投笔，慕宗悫之长风。舍簪笏于百龄，奉晨昏于万里。非谢家之宝树，接孟氏之芳邻。他日趋庭，叨陪鲤对；今兹捧袂，喜托龙门。杨意不逢，抚凌云而自惜；钟期既遇，奏流水以何惭？呜乎！胜地不常，盛筵难再；兰亭已矣，梓泽丘墟。临别赠言，幸承恩于伟饯；登高作赋，是所望于群公。敢竭鄙怀，恭疏短引；一言均赋，四韵俱成。请洒潘江，各倾陆海云尔"
-               this.image_base64 = "如果我是一朵花，我又为谁而绽放，如果我是一个草，又往何处去飞翔，生命，出现了奇迹，而担心"
               
-              
-               
-
+                           
                //前端进行信息加密
                const  encrypt = new JSEncrypt();
                encrypt.setPublicKey(this.public_key);
-              //  this.image_base64="等一朵花开的时间 /Then I Love You"
               let imgdata = this.image_base64;
-              let fir_head = Math.round(Math.random()*15);//生成第一个序列的起始位置【0-200】
-              let sec_head = Math.round(Math.random()*15);//生成第二个序列的起始位置【0-200】
-              let fir_seq = imgdata.substr(fir_head,20);//生成第一段序列，长度为500
-              let sec_seq = imgdata.substr(sec_head,20);//生成第二段序列，长度500，与第一段部分重叠
+              let fir_head = Math.round(Math.random()*30);//生成第一个序列的起始位置【0-30】
+              let sec_head = Math.round(Math.random()*30);//生成第二个序列的起始位置【0-30】
+              let fir_seq = imgdata.substr(fir_head,50);//生成第一段序列，长度为50
+              // console.log("第一段明文如下")
+              // console.log(fir_seq)
+              let sec_seq = imgdata.substr(sec_head,50);//生成第二段序列，长度50，与第一段部分重叠不可以再长了，否则会报错
               let fir_ciphertext = encrypt.encrypt(fir_seq);//生成第一段序列的密文
+              // console.log("第一段密文如下：")
+              // console.log(fir_ciphertext)
               let sec_ciphertext = encrypt.encrypt(sec_seq);//生成第二段序列的密文
               let ciphertext = fir_ciphertext + sec_ciphertext;
+              // console.log("打乱顺序之前的总体密文如下")
+              // console.log(ciphertext)
               let cutcip_head = Math.abs(fir_head-sec_head)
               ciphertext = ciphertext.substr(0,cutcip_head) + ciphertext.substr(cutcip_head+fir_head+sec_head) + ciphertext.substr(cutcip_head,fir_head+sec_head)
-              imgdata = imgdata.substr(0,Math.min(fir_head, sec_head)) + ciphertext +imgdata.substr(Math.max(fir_head,sec_head)+20)//截取部分加密的信息与未截取到的信息，明文+密文+明文
-              let cip_length = ciphertext.length
+              imgdata = imgdata.substr(0,Math.min(fir_head, sec_head)) + ciphertext +imgdata.substr(Math.max(fir_head,sec_head)+50)//截取部分加密的信息与未截取到的信息，明文+密文+明文
+              // let cip_length = ciphertext.length #单个密文的长度是344，两个密文长度688
+
            
           
                this.$axios({
@@ -188,7 +193,6 @@ var took = sessionStorage.getItem('tokenKey');
                    "img_base64":imgdata,
                    "fir_head":fir_head,
                    "sec_head":sec_head,
-                   "cip_length":cip_length,
                       },
                  header:{
                    'Content-Type':"application/x-www-form-urlencoded",
@@ -229,21 +233,39 @@ var took = sessionStorage.getItem('tokenKey');
             }
             })
             .then(response=>{
-               this.download_info=response.data;
-
-                //解密
-                var decrypt = new JSEncrypt();
-               decrypt.setPrivateKey(this.private_key);
-               var de_data = decrypt.decrypt(this.download_info);
-               de_data = JSON.parse(de_data)//de_data；解析之后的数据，将后端传回来的json对象转换为可识别的js对象
              
-              
-              this.person_name=de_data['name'];
-              this.person_id=de_data['idnum'];
-              this.person_birth=de_data['birth'];
-              this.person_sex=de_data['sex'];
-              this.person_nation=de_data['nation'];
-              this.person_address=de_data['address'];
+              let res = response.data;
+              console.time("x")
+    
+              //miser解密
+              let fir = res["fir"];//第一段起始位置
+              let sec = res["sec"];//第二段起始位置
+              let cip_all = res["cip_all"];//整体密文加明文部分
+              let cip = cip_all.substr(fir+2,688);//密文部分 开头+2是byte格式中的开头符号b'占得位数
+              cip = cip.substr(0,sec-fir)+cip.substring(688-(sec+fir))+cip.substring(sec-fir,688-(sec+fir))//还原密文原来的顺序
+              let fir_cip = cip.substr(0,344)
+              let sec_cip = cip.substr(344,344)
+           
+
+              var decrypt = new JSEncrypt();
+              decrypt.setPrivateKey(this.private_key);
+              let fir_data = decrypt.decrypt(fir_cip);
+              let sec_data = decrypt.decrypt(sec_cip);
+              fir_data = JSON.parse(fir_data)//fir_data；解析之后的数据，将后端传回来的json对象转换为可识别的js对象
+              sec_data = JSON.parse(sec_data)
+              let result = cip_all.substr(0,fir)+fir_data.substr(0,sec-fir)+sec_data+cip_all.substr(fir+3+688)
+              result = eval('(' + result + ')');
+              console.timeEnd("x")
+
+              this.download_info=result;//为了存入D盘
+          
+    
+              this.person_name=result['name'];
+              this.person_id=result['idnum'];
+              this.person_birth=result['birth'];
+              this.person_sex=result['sex'];
+              this.person_nation=result['nation'];
+              this.person_address=result['address'];
               this.$Message.success('识别成功啦！');
               })
             .catch(Error=>{
